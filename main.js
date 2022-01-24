17ae5dc5ee3db2bb206d50ad2916919abe9063b6
AKO <arisv.ko@gmail.com>
2022-01-24 20:20:25 +0800

    Implement HTML wave animation

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     lottie.loadAnimation({
-        container: document.getElementById('hero-banner-wave'), // the dom element that will contain the animation
+        container: document.getElementById('wave-1-top'), // the dom element that will contain the animation
         renderer: 'svg',
         loop: true,
         autoplay: true,
         path: './waves/data-color-1.json' // the path to the animation json
     });
     lottie.loadAnimation({
-        container: document.getElementById('hero-banner-wave-2'), // the dom element that will contain the animation
+        container: document.getElementById('wave-1-bottom'), // the dom element that will contain the animation
         renderer: 'svg',
         loop: true,
         autoplay: true,
