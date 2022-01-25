33893c855fb79e7e3deef0d352e4064da8850422
AKO <arisv.ko@gmail.com>
2022-01-25 14:39:27 +0800

    Implemented row 1 and row 2 of home page

--- main.js
+++ main.js
@@ -7,10 +7,17 @@ $(document).ready(function () {
         path: './waves/data-color-1.json' // the path to the animation json
     });
     lottie.loadAnimation({
-        container: document.getElementById('wave-1-bottom'), // the dom element that will contain the animation
+        container: document.getElementById('wave-2-top'), // the dom element that will contain the animation
         renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: './waves/data-color-1.json' // the path to the animation json
+        path: './waves/data-color-2.json' // the path to the animation json
+    });
+    lottie.loadAnimation({
+        container: document.getElementById('wave-3-top'), // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './waves/data-color-3.json' // the path to the animation json
     });
 });
