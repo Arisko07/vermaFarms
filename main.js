01eefe2865910d0bcbe818b68f949adc660c4891
AKO <arisv.ko@gmail.com>
2022-01-25 15:27:34 +0800

    Added 3rd row

--- main.js
+++ main.js
@@ -20,4 +20,11 @@ $(document).ready(function () {
         autoplay: true,
         path: './waves/data-color-3.json' // the path to the animation json
     });
+    lottie.loadAnimation({
+        container: document.getElementById('wave-4-top'), // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: './waves/data-color-2.json' // the path to the animation json
+    });
 });
